bbe59a9944ba58cf943e5d9582aa02b8822ab428
Hong <sr5043@naver.com>
2020-11-19 04:15:15 +0900

    dd

--- dataloader.py
+++ dataloader.py
@@ -3,67 +3,40 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from dataProcess import *
 
-class Drawer():
-    def __init__(self,df,batch_size):
+
+class myDataLoader(Dataset):
+    def __init__(self,df):
         self.df = df
-        self.batch_size = batch_size
-        self.id = 0
+        self.len_idx = 0
         self.id_list = []
-        self.inputTensor = torch.zeros([self.batch_size,7,48])
-        self.targetTensor = torch.zeros([self.batch_size*1*48]) # input은 flatten 해서 dense layer 통과후에 loss 구할거니까 미리 flatten해두자
-        # self.factor
+        self.timestep_list = []
 
-    def dtloader(self,usage):
-        period = [701 if usage=='test' else 202, 730 if usage=='test' else 700]
-        while True:
-            self.id = np.random.randint(1,929+1) # 고객하나에 대해서
-            if self.id in self.id_list:
-                continue
-            else: self.id_list.append(self.id)
-            length = len(self.id_list)
+        #결측치 있는 행 모두 제거
+        self.df = self.df.dropna(axis=0)
 
-            while True:
-                start_day = np.random.randint(period[0],period[1]-6)
-                df_8days = df.loc[self.id,((start_day*100)+1):start_day*100+748] # target까지 뽑는건 8일치를 뽑는다
-                if df_8days.isnull().values.any() == True: # 시계열에 결측치 있으면 다시 뽑는다
-                    continue
-                else: # 아니라면 7일치를 tensor에 저장
-                    s = 0
-                    e = 48
-                    for i in range(8):
-                        for j in range(s,e):
-                            self.inputTensor[length,i,np.asarray(df_8days)[j]] # 외 않돼는지 몰르갯다
-                        s += 48
-                        e += 48
-                    self.targetTensor[(length-1)*48:length*48] = np.asarray(df_8days)[336:384]
-                    break
+        self.id_list = self.df.index
+        self.timestep_list = list(self.df.iloc[0:,-1].values)
 
-                    
+    def __len__(self):
+        return(len(self.df.index))
 
-            # timstep으로 월 일 주 factor추출(앞의 7일만). 후에 tensor로 만듦
-            # id tensor로 만듦
-            # 위 둘을 concat
-            
-            if len(self.id_list)==self.batch_size:
-                break
-            else: pass
-        # 8일차를 target으로
-        # 나머지는 input data로
-        # input data, target, factor를 return
-        #return data,target,factor
-        return self.inputTensor, self.targetTensor #,self.factor
+    def __getitem__(self,index):
+        x = torch.tensor(self.df.iloc[index,0*48:7*48]).view(1,7,48)
+        y = torch.tensor(self.df.iloc[index,7*48:8*48]).view(1,1,48)
+        factor = getFactorTensor(self.id_list[index],self.timestep_list[index])
+
+        return x, y, factor
 
 
 
 if __name__ == "__main__":
-    df = loadData("data/df_timeSeries.csv")
-    df = renameRow(renameCol(df))
+    df_test = df_test = pd.read_csv("/daintlab/data/sr/testdf.csv",index_col=0)
+    
+    test_dataset = myDataLoader(df_test)
+    test_loader = DataLoader(test_dataset, shuffle=False, batch_size=64, pin_memory=False)
 
-    loader = Drawer(df,64)
-    #train_data, train_target, train_factor = loader.dtloader(train)
-    #test_data, test_target, test_factor = loader.dtloader(test)
-    #val_data, val_target, val_factor = laoder.dtloader(val)
+    x, y, factor = next(iter(test_loader))
 
-    inputTensor, targetTensor = loader.dtloader('train')
-    print('=======▼input=======',inputTensor,'\n','=======▼target=======')
-    print('\n','=======▼customer ID=======',loader.id_list)
+    print("x :",x.shape)
+    print("y :",y.shape)
+    print("factor :",factor.shape)
--- traintestmaker.py
+++ traintestmaker.py
@@ -2,43 +2,53 @@ from dataProcess import *
 import torch
 import pandas as pd
 import numpy as np
+import time
 
 
 
 def mkTraindata(df,dir):
-    df = df.iloc[0:,0:506*48]
-    df_train = pd.DataFrame(index=[(i//499)+1 for i in range(929*499)],columns=[j for j in range(48*8+1)])
-    len_idx = len(df.index)
-    len_col = len(df.columns)
-    for i in range(len_idx):
-
-    df_train.to_csv(dir,header=True,index=)
+    df1 = df.iloc[0:,(8-1)*48:(8-1+8)*48]
+    for i in range(1,492):
+        df2 = df.iloc[0:,(8-1+i)*48:(8-1+i+8)*48]
+        df1 = pd.DataFrame(np.concatenate([df1.values, df2.values]), columns=df1.columns)
+    df1.columns = [i for i in range(0,384)]
+    df1.index = [(j%929)+1 for j in range(0,457068)]
+    #col 하나 추가해서 time넣어줘야한다
+    df1.to_csv(dir+"/traindf.csv",header=True,index=True)
 
 
 def mkTestdata(df,dir):
-    df = df.iloc[0:,(507-1)*48:]
-    df_test = pd.DataFrame(index=[(i//23)+1 for i in range(929*23)],columns=[j for j in range(48*8+1)])
-    len_idx = len(df.index)
-    len_col = len(df.columns)
-    for i in range(len_idx):
+    df1 = df.iloc[0:,(507-1)*48:(507-1+8)*48]
+    for i in range(1,23):
+        df2 = df.iloc[0:,(507-1+i)*48:(507-1+i+8)*48]
+        df1 = pd.DataFrame(np.concatenate([df1.values, df2.values]), columns=df1.columns)
+    df1.columns = [i for i in range(0,384)]
+    df1.index = [(j%929)+1 for j in range(0,21367)]
+    #col 하나 추가해서 time넣어줘야한다
+    df1.to_csv(dir+"/testdf.csv",header=True,index=True)
 
-    df_test.to_csv(dir,header=True,index=)
 
     
 
 
 
 if __name__ == "__main__":
+    start = time.time()
+
     df = loadData("/daintlab/data/sr/df_timeSeries.csv")
     df = renameCol(df) # 536일
     df = renameRow(df)
+    print('\n\n== df loaded ==\n\n')
 
-
-    '''
     print("==mkTraindata==")
-    mkTestdata(df,'/daintlab/data/sr/')
+    mkTraindata(df,"/daintlab/data/sr")
     print("==done==")
+    print(time.time()-start)
+
+    start = time.time()
     print("==mkTestdata==")
-    mkTestdata(df,'/daintlab/data/sr/')
+    mkTestdata(df,"/daintlab/data/sr")
     print("==done==")
-    '''
+    print(time.time()-start)
+
+    # df_test = pd.read_csv("/daintlab/data/sr/testdf.csv",index_col=0) 불러올때는 이렇게
